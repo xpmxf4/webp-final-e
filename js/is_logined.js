8f51b03b126bf24268484ece62ef5c984b06a3ac
unknown <xpmxf5@gmail.com>
2022-06-20 19:33:29 +0900

    login 시 동적으로 이름 변하기

--- js/is_logined.js
+++ js/is_logined.js
@@ -5,27 +5,34 @@ Kakao.init("d7583032b88962c538ab3eed34ef8315");
 const login_cpn = document.getElementById("login");
 
 if (Kakao.Auth.getAccessToken()) {
-    login_cpn.innerHTML =
-        "<a>위시리스트</a><a>내 여행</a><a>메시지</a><a>알림</a><img id='user' src='images/profile_img.png' /><div id='profile_popup'><div id='popup_first_row' class='popup_rows'><span class='name'>박채훈</span><span class='profile_manage'>프로필 관리</span></div><div class='popup_rows'><p>포인트</p><p>쿠폰</p></div><div class='popup_rows'><p>친구 초대</p></div><div class='popup_rows'><p>파트너 등록하기</p><p id='logout'>로그아웃</p></div></div>";
+    Kakao.API.request({
+        url: "/v2/user/me",
+        success: function (res) {
+            login_cpn.innerHTML = `<a>위시리스트</a><a>내 여행</a><a>메시지</a><a>알림</a><img id='user' src='images/profile_img.png' /><div id='profile_popup'><div id='popup_first_row' class='popup_rows'><span class='name'>${res.properties.nickname}</span><span class='profile_manage'>프로필 관리</span></div><div class='popup_rows'><p>포인트</p><p>쿠폰</p></div><div class='popup_rows'><p>친구 초대</p></div><div class='popup_rows'><p>파트너 등록하기</p><p id='logout'>로그아웃</p></div></div>`;
+
+            // 드랍다운
+            const img = document.getElementById("user");
+            const popup = document.getElementById("profile_popup");
+            const logout_btn = document.getElementById("logout");
+
+            img.addEventListener("click", function () {
+                popup.style.display = "inline-block";
+            });
+
+            popup.addEventListener("mouseleave", function () {
+                popup.style.display = "none";
+            });
+
+            // 로그아웃
+            logout_btn.addEventListener("click", unlinkApp);
+        },
+        fail: function (error) {
+            console.log(error);
+        },
+    });
 } else {
     login_cpn.innerHTML =
-        "<a href=''><div class='notSignup'>파트너 등록하기</div></a><a href='login/login.html'><div class='notSignup'>로그인</div></a><div id='signup-box'><a id='signup' href=''><div>회원가입</div></a></div> ";
-}
-
-// 드랍 다운
-if (login_cpn.textContent.includes("위시리스트")) {
-    const img = document.getElementById("user");
-    const popup = document.getElementById("profile_popup");
-
-    img.addEventListener("click", () => {
-        popup.style.display = "inline-block";
-    });
-
-    if (popup.style.display == "inline-block" && e.e.target.id != profile_popup) {
-        document.addEventListener("click", (e) => {
-            popup.style.display = "none";
-        });
-    }
+        "<a href=''><div class='notSignup'>파트너 등록하기</div></a><a href='login/login.html'><div class='notSignup'>로그인</div></a><div id='signup-box'><a id='signup' href=''><div>회원가입</div></a></div>";
 }
 
 // 로그아웃
@@ -45,6 +52,4 @@ function unlinkApp() {
     });
 }
 
-const logout_btn = document.getElementById("logout");
 
-logout_btn.addEventListener("click", unlinkApp);
