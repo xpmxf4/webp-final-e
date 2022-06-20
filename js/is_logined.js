0117cd1d160b153b9e2c8110aa8c8d35767ff457
unknown <xpmxf5@gmail.com>
2022-06-20 12:36:50 +0900

    기타 html href, css 수정

--- js/is_logined.js
+++ js/is_logined.js
@@ -6,10 +6,10 @@ const login_cpn = document.getElementById("login");
 
 if (Kakao.Auth.getAccessToken()) {
     login_cpn.innerHTML =
-        "<a>위시리스트</a><a>내 여행</a><a>메시지</a><a>알림</a><img id='user' src='./images/profile_img.png' /><div id='profile_popup'><div id='popup_first_row' class='popup_rows'><span class='name'>박채훈</span><span class='profile_manage'>프로필 관리</span></div><div class='popup_rows'><p>포인트</p><p>쿠폰</p></div><div class='popup_rows'><p>친구 초대</p></div><div class='popup_rows'><p>파트너 등록하기</p><p id='logout'>로그아웃</p></div></div>";
+        "<a>위시리스트</a><a>내 여행</a><a>메시지</a><a>알림</a><img id='user' src='../images/profile_img.png' /><div id='profile_popup'><div id='popup_first_row' class='popup_rows'><span class='name'>박채훈</span><span class='profile_manage'>프로필 관리</span></div><div class='popup_rows'><p>포인트</p><p>쿠폰</p></div><div class='popup_rows'><p>친구 초대</p></div><div class='popup_rows'><p>파트너 등록하기</p><p id='logout'>로그아웃</p></div></div>";
 } else {
     login_cpn.innerHTML =
-        "<a href=''><div class='notSignup'>파트너 등록하기</div></a><a href='./login.html'><div class='notSignup'>로그인</div></a><div id='signup-box'><a id='signup' href=''><div>회원가입</div></a></div> ";
+        "<a href=''><div class='notSignup'>파트너 등록하기</div></a><a href='./login/login.html'><div class='notSignup'>로그인</div></a><div id='signup-box'><a id='signup' href=''><div>회원가입</div></a></div> ";
 }
 
 // 드랍 다운
